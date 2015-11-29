0f1f7c64cb3a16580cf419f156e8964520db83b0
Xiang Zhang <zxteloiv@gmail.com>
2015-11-29 18:11:40 +0800

    fix bug that not correctly save scraped data

--- xiachufang/settings.py
+++ xiachufang/settings.py
@@ -24,9 +24,9 @@ NEWSPIDER_MODULE = 'xiachufang.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN=16
+CONCURRENT_REQUESTS_PER_DOMAIN=2
 #CONCURRENT_REQUESTS_PER_IP=16
 
 # Disable cookies (enabled by default)
--- xiachufang/spiders/recipe.py
+++ xiachufang/spiders/recipe.py
@@ -14,7 +14,7 @@ class RecipeSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        if re.match('/recipe/', response.url):
+        if re.search('/recipe/', response.url):
             yield self.parse_recipe(response)
 
         # follow links
@@ -29,23 +29,23 @@ class RecipeSpider(scrapy.Spider):
         item = RecipeItem()
         # use the max length text with the attribute *itemprop*
         name_xpath = '//*[@itemprop="name"]/text()'
-        item.name = max(response.xpath(name_xpath).extract(), key = len).strip()
+        item["name"] = max(response.xpath(name_xpath).extract(), key = len).strip()
 
         # the rating value for this recipe
         rating_xpath = '//*[@itemprop="ratingValue"]/text()'
-        item.rating = float(response.xpath(rating_xpath).extract()[0])
+        item["rating"] = float(response.xpath(rating_xpath).extract()[0])
 
         # how many users have rated this recipe
         rating_num_xpath = '//*[@itemprop="ratingCount"]/text()'
-        item.rating_num = int(response.xpath(rating_num_xpath).extract()[0])
+        item["rating_num"] = int(response.xpath(rating_num_xpath).extract()[0])
 
         # how many users have used this recipe to cook
         tried_num_xpath = '//*[@class="cooked"]//*[@class="number"]/text()'
-        item.tried_num = int(response.xpath(tried_num_xpath).extract()[0])
+        item["tried_num"] = int(response.xpath(tried_num_xpath).extract()[0])
 
         # description for this recipe
         desc_xpath = '//*[@itemprop="description"]/text()'
-        item.desc = "\n".join(x.strip() for x in response.xpath(desc_xpath).extract())
+        item["desc"] = "\n".join(x.strip() for x in response.xpath(desc_xpath).extract())
 
         # ingredients in this recipe
         for sel in response.xpath('//*[@itemprop="ingredients"]'):
@@ -53,23 +53,23 @@ class RecipeSpider(scrapy.Spider):
             name_sel = sel.xpath('*[contains(@class,"name")]')
             unit_sel = sel.xpath('*[contains(@class,"unit")]')
             
-            item.ingredients = dict(izip(
+            item["ingredients"] = dict(izip(
                 (re.sub('<[^>]*>', '', x).strip() for x in name_sel.extract()),
                 (re.sub('<[^>]*>', '', x).strip() for x in unit_sel.extract()),
                 ))
 
         # steps to cook
         step_xpath = '//div[@class="steps"]/descendant::p[@class="text"]/text()'
-        item.steps = [x.strip() for x in response.xpath(step_xpath).extract()]
+        item["steps"] = [x.strip() for x in response.xpath(step_xpath).extract()]
 
         # tips content
         tips_xpath = '//div[@class="tip"]/text()'
         if tips_xpath:
-            item.tips = "\n".join(x.strip() for x in response.xpath(tips_xpath).extract())
+            item["tips"] = "\n".join(x.strip() for x in response.xpath(tips_xpath).extract())
 
-        item.url = response.url
-        item.crawl_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        item.recipe_id = re.search('(\d+)', response.url).groups(0)[0]
+        item["url"] = response.url
+        item["crawl_date"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        item["recipe_id"] = re.search('(\d+)', response.url).groups(0)[0]
 
         return item
 
